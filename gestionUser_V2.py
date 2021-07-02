5df547454852ad33f84ccb988a1a643bf875af7a
Matteo Lopes <matteo_lopes@outlook.fr>
2021-07-02 14:06:46 +0200

    Version 2: ajout de la gestion mdp et ajout des valeurs dynamique

---
+++ gestionUser_V2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import mysql.connector
+import sys
+import hashlib
+
+############## Création de la table si elle n'existe pas ############
+
+sql_create = """
+CREATE TABLE IF NOT EXISTS User ( 
+   iduser int(5) NOT NULL,
+   firstname varchar(10) NOT NULL, 
+   lastname varchar(10) DEFAULT NULL, 
+   role varchar(2) DEFAULT NULL,
+   password varchar(250) DEFAULT NULL,
+   PRIMARY KEY(iduser) ); """
+
+############## Création de l'utilisateur avec gestion du mot de passe ############
+
+user={}
+print("Création d'un utilisateur : ")
+print("")
+user["iduser"]=(input("Id User : "))
+print("")
+user["firstname"]=(input("Prénom : "))
+print("")
+user["lastname"]=(input("Nom : "))
+print("")
+user["role"]=(input("Role : "))
+print("")
+
+mdp=input("Entrez le mot de passe:")
+mdp_hashe=hashlib.md5(mdp.encode()).hexdigest()
+user["password"]=(mdp_hashe)
+print(user)
+
+############## Ajout de l'utilisateur dans la base de donnée ############
+
+try:
+   conn = mysql.connector.connect(host="localhost", user="root", password="", database="bibliotheque")
+   cursor = conn.cursor()
+   cursor.execute(sql_create)
+   print("Connexion à la base de donnée Réussi\n")
+   
+   try:
+      print(user)
+      #user = {'iduser': 12345, 'firstname' : "Matteo", 'lastname' : "Lopes", 'role' : "AS"} 
+      cursor.execute("""INSERT INTO User (iduser, firstname, lastname, role, password) VALUES(%(iduser)s, %(firstname)s, %(lastname)s, %(role)s, %(password)s)""", user)
+      conn.commit()
+      print("Utilisateur Crée avec succès\n")
+   except:
+      # En cas d'erreur on annule les modifications
+      conn.rollback()
+except mysql.connector.errors.InterfaceError as e:
+   print("Error %d: %s" % (e.args[0],e.args[1]))
+   sys.exit(1)
+finally:
+   # On ferme la connexion
+   if conn:
+      conn.close()
+
